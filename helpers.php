44372c758a3cf0b7bdda894a6252327ada2b7d50
Ali Albanna <ali@albanna.com>
2024-05-15 19:40:28 +0300

    initial commit, files setup

---
+++ helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Get the base path 
+ * 
+ * @param string $path
+ * @return string
+ */
+
+function basePath($path = "") {
+  // echo __DIR__ . '/' . $path;
+  return __DIR__ . '/' . $path;
+}
