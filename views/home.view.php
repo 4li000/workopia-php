04dc71f65bfef30f73c78617d76ae7b52bc07b0b
Ali Albanna <ali@albanna.com>
2024-05-15 20:30:11 +0300

    top-banner partials

--- views/home.view.php
+++ views/home.view.php
@@ -4,16 +4,8 @@
 <?php require basePath('views/partials/navbar.php') ?>
 <!-- Showcase -->
 <?php require basePath('views/partials/showcase-search.php') ?>
-
 <!-- Top Banner -->
-<section class="bg-blue-900 text-white py-6 text-center">
-  <div class="container mx-auto">
-    <h2 class="text-3xl font-semibold">Unlock Your Career Potential</h2>
-    <p class="text-lg mt-2">
-      Discover the perfect job opportunity for you.
-    </p>
-  </div>
-</section>
+<?php require basePath('views/partials/top-banner.php') ?>
 
 <!-- Job Listings -->
 <section>
