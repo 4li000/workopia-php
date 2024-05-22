bcc694331461bcad7fe6cdae6cd1d2a0a096bd0f
4li000 <4li.albanna@gmail.com>
2024-05-22 03:38:00 +0300

    403, 404 refactore of raouts

---
+++ views/error/403.view.php
@@ -0,0 +1,16 @@
+<!-- Header -->
+<?php loadPartial('head') ?>
+<!-- Nav -->
+<?php loadPartial('navbar') ?>
+
+<section>
+  <div class="container mx-auto p-4 mt-4">
+    <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">403 Error</div>
+    <p class="text-center text-2xl mb-4">
+      Your are not authorized to view this page
+    </p>
+  </div>
+</section>
+
+<!-- Footer -->
+<?php loadPartial('footer') ?>
